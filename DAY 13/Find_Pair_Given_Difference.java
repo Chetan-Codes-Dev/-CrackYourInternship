9ed926dc9cbb37cc1e13afaef8996003bed38ca4
Chetan-Codes-Dev <ultronicity@gmail.com>
2024-08-05 18:41:52 +0530

    Add files via upload

---
+++ DAY 13/Find_Pair_Given_Difference.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findPairs(int[] nums, int k) {
+       
+        int n = nums.length;
+        Arrays.sort(nums);
+       
+        int  i =0;
+        int j = i+1;
+int findpairs = 0;
+        while(j<n && i<n ){
+            if(i==j){
+                j++;
+            }
+            else{
+            if(nums[j]-nums[i]==k){
+                findpairs++;
+                i++;
+                while (i < n && nums[i] == nums[i - 1]){
+            i++;}
+            }else if(nums[j]-nums[i]>k){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        }
+        return findpairs;
+    }
+}
